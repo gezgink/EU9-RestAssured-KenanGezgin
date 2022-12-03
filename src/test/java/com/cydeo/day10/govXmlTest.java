57a73e09a445dbb9e8889ef0e3a5d05c285cbc7f
kenan <gezginkenan@gmail.com>
2022-12-03 09:49:20 +0300

    Day10 Gov Xml Test

---
+++ src/test/java/com/cydeo/day10/govXmlTest.java
@@ -0,0 +1,49 @@
+package com.cydeo.day10;
+
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.get;
+
+public class govXmlTest {
+
+
+
+    @Test
+    public void test1(){
+        //send a get request to
+        //https://data.ct.gov/api/views/qm34-pq7e/rows.xml
+        //get all the years
+        //get all unknowns
+        //get 2005 other
+        //get 2007 _address
+        Response response = get("https://data.ct.gov/resource/y6p2-px98.xml")
+                            .then().statusCode(200).extract().response();
+
+        XmlPath xmlPath = response.xmlPath();
+
+        //get all the category
+        List<String> category = xmlPath.getList("response.rows.row.category");
+        System.out.println("category = " + category);
+        //get all unknowns
+        List<String> item = xmlPath.getList("response.rows.row.item");
+        System.out.println("item = " + item);
+
+//        //get 2005 other
+//        int locationZip = xmlPath.getInt("response.rows.row.location_1_zip");
+//        System.out.println("locationZip = " + locationZip);
+//        //get 2007 _address
+//        String adress = xmlPath.getString("response.rows.row.location_1_address");
+//        System.out.println("adress = " + adress);
+
+
+
+
+        //Not: Jamal'ın linki çalışmıyordu. Yeni paylaşılan link üzerinden verileri değiştirirerek yaptım.
+
+
+    }
+}
